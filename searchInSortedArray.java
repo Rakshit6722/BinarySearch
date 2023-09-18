92daaee58afa6e772b8c3550dd15a6cc11ef1595
Rakshit6722 <rakshitchauhan435@gmail.com>
2023-09-18 18:15:06 +0530

    binary search in rotated array

--- searchInSortedArray.java
+++ searchInSortedArray.java
@@ -2,7 +2,9 @@ package binarySearch;
 
 public class searchInSortedArray {
     public static void main(String[] args) {
-
+        int[] arr = {4,5,6,7,0,1,2};
+        int target = 0;
+        System.out.println(search(arr, target));
     }
 
     public static int search(int[] nums, int target) {
