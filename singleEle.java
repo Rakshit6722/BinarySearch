c4f1fc2679e4cb944947b2476fbdc402940f186d
Rakshit6722 <rakshitchauhan435@gmail.com>
2023-09-18 22:02:45 +0530

    brute force

--- singleEle.java
+++ singleEle.java
@@ -3,7 +3,28 @@ package binarySearch;
 public class singleEle {
     public static void main(String[] args) {
         int[] arr = {1,1,2,2,4,5,5};
+        System.out.println(singleNum(arr));
         System.out.println(singleNonDuplicate(arr));
+    }
+
+    //brute force
+    public static int singleNum(int[] arr){
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            if(i==0){//first element
+                if(arr[i]!=arr[i+1]) return arr[i];
+            } 
+            if(i==n-1){//last element
+                if(arr[i]!=arr[i-1]) return arr[i];
+            }
+            else{//rest elements
+                if(arr[i]!=arr[i+1] && arr[i]!=arr[i-1]){
+                    return arr[i];
+                }
+            }
+        
+        }
+        return -1;
     }
 
     //method - 1 (XOR)
