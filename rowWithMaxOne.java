cd358f5f0dac3a236092a07a7c8965d5969a6d58
Rakshit6722 <rakshitchauhan435@gmail.com>
2023-09-25 11:50:24 +0530

    2D array BS

--- rowWithMaxOne.java
+++ rowWithMaxOne.java
@@ -37,18 +37,55 @@ public class rowWithMaxOne {
 
 
     //brute force
-    public static int maximumOnesRow(ArrayList<ArrayList<Integer>> matrix, int n, int m) {
+    // public static int maximumOnesRow(ArrayList<ArrayList<Integer>> matrix, int n, int m) {
         
+    //     int idx = -1;
+    //     int max_cnt = -1;
+
+    //     for (int i = 0; i < n; i++) {//iterating from 1st row to last
+    //         int cnt_one = 0;
+    //         for (int j = 0; j < m; j++) {//iterating from 1st col to last
+    //             cnt_one += matrix.get(i).get(j);//finding total no of one's in the row
+    //         }
+    //         if (cnt_one > max_cnt) {
+    //             max_cnt = cnt_one;
+    //             idx = i;
+    //         }
+    //     }
+    //     return idx;
+    // }
+
+    //Optimal soln using binary search (Lower Bound)
+
+    //to find lower bound of curr row
+    public static int lowerBound(ArrayList<Integer> arr, int n, int x){
+        int s = 0;
+        int e = n-1;
+        int ans = n;
+
+        while(s<=e){
+            int mid = s + (e-s)/2;
+
+            if(arr.get(mid)>=x){
+                ans = mid;
+                e = mid - 1;
+            }
+            else{
+                s = mid + 1;
+            }
+        }
+        return ans;
+    }
+    public static int maximumOnesRow(ArrayList<ArrayList<Integer>> matrix, int n, int m)
+    {
+        //	  Write your code here.
         int idx = -1;
         int max_cnt = -1;
 
-        for (int i = 0; i < n; i++) {//iterating from 1st row to last
-            int cnt_one = 0;
-            for (int j = 0; j < m; j++) {//iterating from 1st col to last
-                cnt_one += matrix.get(i).get(j);//finding total no of one's in the row
-            }
-            if (cnt_one > max_cnt) {
-                max_cnt = cnt_one;
+        for(int i=0;i<n;i++){
+            int cnt_one = m - lowerBound(matrix.get(i), m, 1); //no of one will be equal to col length - lower bound of 1 in the current row
+            if(cnt_one>max_cnt){
+                max_cnt=cnt_one;
                 idx = i;
             }
         }
