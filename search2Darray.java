c97114202233149a0a1df08db31d733e9fbb9931
Rakshit6722 <rakshitchauhan435@gmail.com>
2023-09-25 12:00:13 +0530

    2D array BS

--- search2Darray.java
+++ search2Darray.java
@@ -8,14 +8,39 @@ public class search2Darray {
     }
 
     //linear search
+    // public static boolean searchMatrix(int[][] matrix, int target) {
+    //     int n = matrix.length;
+    //     int m = matrix[0].length;
+
+    //     for(int i=0;i<n;i++){
+    //         for(int j=0;j<m;j++){
+    //             if(matrix[i][j]==target) return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+
+    //using binary search in each row
+    public static boolean searchRow(int[] arr, int target){
+        int s = 0;
+        int e = arr.length - 1;
+
+        while(s<=e){
+            int mid = s + (e-s)/2;
+
+            if(arr[mid]==target) return true;
+
+            else if(arr[mid]>target) e = mid - 1;
+            else s = mid + 1;
+        }
+        return false;
+    } 
     public static boolean searchMatrix(int[][] matrix, int target) {
         int n = matrix.length;
         int m = matrix[0].length;
 
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==target) return true;
-            }
+            if(searchRow(matrix[i],target)) return true;  //applying binary search in each row  
         }
         return false;
     }
